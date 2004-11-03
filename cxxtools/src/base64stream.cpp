83276576be522b61ed667a58f7387089c868d382
tommi <tommi@15ae5fad-cc11-0410-8fac-bce609e504b0>
2004-11-03 19:38:36 +0000

    Fehler im include
    git-svn-id: svn://svn.code.sf.net/p/cxxtools/code/trunk@70 15ae5fad-cc11-0410-8fac-bce609e504b0

--- cxxtools/src/base64stream.cpp
+++ cxxtools/src/base64stream.cpp
@@ -19,7 +19,7 @@ Foundation, Inc., 59 Temple Place, Suite 330,
 Boston, MA  02111-1307  USA
 */
 
-#include "base64stream.h"
+#include "cxxtools/base64stream.h"
 #include <map>
 
 static char cv[65] = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/";
