7523c5784e2178fcae4af10c2582c7b980087dae
tommi <tommi@15ae5fad-cc11-0410-8fac-bce609e504b0>
2004-12-03 20:51:15 +0000

    demo-programm for logging
    git-svn-id: svn://svn.code.sf.net/p/cxxtools/code/trunk@89 15ae5fad-cc11-0410-8fac-bce609e504b0

---
+++ cxxtools/demo/log.cpp
@@ -0,0 +1,22 @@
+#include <cxxtools/log.h>
+
+log_define("log-demo");
+
+int main(int argc, char* argv[])
+{
+  try
+  {
+    log_init();
+
+    log_fatal("fatal message");
+    log_error("error message");
+    log_warn("warn message");
+    log_info("info message");
+    log_debug("debug message");
+  }
+  catch (const std::exception& e)
+  {
+    std::cerr << e.what() << std::endl;
+    return -1;
+  }
+}
