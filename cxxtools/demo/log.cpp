2e77e614b33bd49e4720f042659768559902535c
tommi <tommi@15ae5fad-cc11-0410-8fac-bce609e504b0>
2005-01-05 07:30:24 +0000

    *** empty log message ***
    git-svn-id: svn://svn.code.sf.net/p/cxxtools/code/trunk@118 15ae5fad-cc11-0410-8fac-bce609e504b0

--- cxxtools/demo/log.cpp
+++ cxxtools/demo/log.cpp
@@ -3,13 +3,15 @@
 #include <iostream>
 #include <stdexcept>
 
-log_define("log");  // global definition
+log_define("global");  // global definition
 
 namespace ns
 {
-  log_define("log.ns");  // namespace definition
+  log_define("namespace");  // namespace scope definition
   void main()
   {
+    // this is logged at category "namespace"
+
     log_trace("main");
 
     log_fatal("fatal message in namespace ns");
@@ -27,7 +29,7 @@ class cl
   public:
     cl()
     {
-      log_debug("constructor here");
+      log_debug("constructor here");  // this is logged at category "class"
     }
     ~cl()
     {
@@ -41,6 +43,7 @@ int main(int argc, char* argv[])
   {
     log_init();
 
+    // this is logged at category "global"
     log_fatal("fatal message " << 1);
     log_error("error message " << 2);
     log_warn("warn message " << 3);
