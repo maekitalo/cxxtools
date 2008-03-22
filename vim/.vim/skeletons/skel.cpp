29a6d59ac1cf86d59eff60af7654ec5535708906
maekitalo <maekitalo@15ae5fad-cc11-0410-8fac-bce609e504b0>
2008-03-22 10:26:29 +0000

    update copyright year
    git-svn-id: svn://svn.code.sf.net/p/cxxtools/code/trunk@489 15ae5fad-cc11-0410-8fac-bce609e504b0

--- vim/.vim/skeletons/skel.cpp
+++ vim/.vim/skeletons/skel.cpp
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2007 Tommi Maekitalo
+ * Copyright (C) 2008 Tommi Maekitalo
  *
  * This program is free software; you can redistribute it and/or
  * modify it under the terms of the GNU General Public License as
