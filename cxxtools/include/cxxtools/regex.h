1defa422bd8f493f645214f12a7198e1990edf82
maekitalo <maekitalo@15ae5fad-cc11-0410-8fac-bce609e504b0>
2008-05-04 21:34:36 +0000

    new wrapper class for regex(3)
    git-svn-id: svn://svn.code.sf.net/p/cxxtools/code/trunk@534 15ae5fad-cc11-0410-8fac-bce609e504b0

---
+++ cxxtools/include/cxxtools/regex.h
@@ -0,0 +1,101 @@
+/* cxxtools/regex.h
+ * Copyright (C) 2005-2008 Tommi Maekitalo
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
+ *
+ */
+
+
+#ifndef CXXTOOLS_REGEX_H
+#define CXXTOOLS_REGEX_H
+
+#include <string>
+#include <sys/types.h>
+#include <regex.h>
+#include <cxxtools/smartptr.h>
+
+namespace cxxtools
+{
+  class RegexSMatch;
+
+  template <typename objectType>
+  class RegexDestroyPolicy;
+
+  template <>
+  class RegexDestroyPolicy<regex_t>
+  {
+    protected:
+      void destroy(regex_t* expr)
+      {
+        ::regfree(expr);
+        delete expr;
+      }
+  };
+
+  /// regex(3)-wrapper.
+  class Regex
+  {
+      SmartPtr<regex_t, ExternalRefCounted, RegexDestroyPolicy> expr;
+
+      void checkerr(int ret) const;
+
+    public:
+      explicit Regex(const char* ex, int cflags = REG_EXTENDED)
+        : expr(new regex_t())
+      {
+        checkerr(::regcomp(expr.getPointer(), ex, cflags));
+      }
+
+      explicit Regex(const std::string& ex, int cflags = REG_EXTENDED)
+        : expr(new regex_t())
+      {
+        checkerr(::regcomp(expr.getPointer(), ex.c_str(), cflags));
+      }
+
+      bool match(const std::string& str_, RegexSMatch& smatch, int eflags = 0) const;
+      bool match(const std::string& str_, int eflags = 0) const;
+
+      void free()  { expr = 0; }
+  };
+
+  /// collects matches in a regex
+  class RegexSMatch
+  {
+      friend class Regex;
+
+      std::string str;
+      regmatch_t matchbuf[10];
+
+    public:
+      /// returns the number of expressions, which were found
+      unsigned size() const;
+      /// returns the start position of the n-th expression
+      regoff_t offsetBegin(unsigned n) const   { return matchbuf[n].rm_so; }
+      /// returns the end position of the n-th expression
+      regoff_t offsetEnd(unsigned n) const     { return matchbuf[n].rm_eo; }
+
+      /// returns the n-th element. No range checking is done.
+      std::string get(unsigned n) const;
+      /// replace each occurence of "$n" with the n-th element (n: 0..9).
+      std::string format(const std::string& s) const;
+      /// returns the n-th element. No range checking is done.
+      std::string operator[] (unsigned n) const
+        { return get(n); }
+  };
+
+}
+
+#endif // CXXTOOLS_REGEX_H
+
---
+++ cxxtools/src/regex.cpp
@@ -0,0 +1,165 @@
+/* regex.cpp
+ * Copyright (C) 2005-2008 Tommi Maekitalo
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
+ *
+ */
+
+
+#include "cxxtools/regex.h"
+#include <stdexcept>
+#include <locale>
+#include <cctype>
+
+namespace cxxtools
+{
+  unsigned RegexSMatch::size() const
+  {
+    unsigned n;
+    for (n = 0; n < 10 && matchbuf[n].rm_so >= 0; ++n)
+      ;
+
+    return n;
+  }
+
+  std::string RegexSMatch::get(unsigned n) const
+  {
+    return str.substr(matchbuf[n].rm_so, matchbuf[n].rm_eo - matchbuf[n].rm_so);
+  }
+
+  std::string RegexSMatch::format(const std::string& s) const
+  {
+    enum state_type
+    {
+      state_0,
+      state_esc,
+      state_var0,
+      state_var1,
+      state_1
+    } state;
+
+    state = state_0;
+    std::string ret;
+
+    for (std::string::const_iterator it = s.begin(); it != s.end(); ++it)
+    {
+      char ch = *it;
+
+      switch (state)
+      {
+        case state_0:
+          if (ch == '$')
+            state = state_var0;
+          else if (ch == '\\')
+            state = state_esc;
+          break;
+
+        case state_esc:
+          ret += ch;
+          state = state_1;
+          break;
+
+        case state_var0:
+          if (std::isdigit(ch))
+          {
+            ret = std::string(s.begin(), it - 1);
+            regoff_t s = matchbuf[ch - '0'].rm_so;
+            regoff_t e = matchbuf[ch - '0'].rm_eo;
+            if (s >= 0 && e >= 0)
+              ret.append(str, s, e-s);
+            state = state_1;
+          }
+          else
+            state = state_0;
+          break;
+
+        case state_1:
+          if (ch == '$')
+            state = state_var1;
+          else if (state == '\\')
+            state = state_esc;
+          else
+            ret += ch;
+          break;
+
+        case state_var1:
+          if (std::isdigit(ch))
+          {
+            unsigned s = matchbuf[ch - '0'].rm_so;
+            unsigned e = matchbuf[ch - '0'].rm_eo;
+            if (s >= 0 && e >= 0)
+              ret.append(str, s, e-s);
+            state = state_1;
+          }
+          else if (ch == '$')
+            ret += '$';
+          else
+          {
+            ret += '$';
+            ret += ch;
+          }
+          break;
+      }
+    }
+
+    switch (state)
+    {
+      case state_0:
+      case state_var0:
+        return s;
+
+      case state_esc:
+        return ret + '\\';
+
+      case state_var1:
+        return ret + '$';
+
+      case state_1:
+        return ret;
+    }
+
+    return ret;
+  }
+
+  void Regex::checkerr(int ret) const
+  {
+    if (ret != 0)
+    {
+      char errbuf[256];
+      regerror(ret, expr.getPointer(), errbuf, sizeof(errbuf));
+      throw std::runtime_error(errbuf);
+    }
+  }
+
+  bool Regex::match(const std::string& str_, int eflags) const
+  {
+    RegexSMatch smatch;
+    return match(str_, smatch, eflags);
+  }
+
+  bool Regex::match(const std::string& str_, RegexSMatch& smatch, int eflags) const
+  {
+    smatch.str = str_;
+    int ret = regexec(expr.getPointer(), str_.c_str(),
+        sizeof(smatch.matchbuf) / sizeof(regmatch_t), smatch.matchbuf, eflags);
+
+    if (ret ==REG_NOMATCH)
+      return false;
+
+    checkerr(ret);
+    return true;
+  }
+
+}
