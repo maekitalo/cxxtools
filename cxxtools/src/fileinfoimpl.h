76cf38920d5189713edc52ee28a75556c0b6e3b0
d-marc <d-marc@15ae5fad-cc11-0410-8fac-bce609e504b0>
2008-06-09 08:23:23 +0000

    added new file/dir API
    git-svn-id: svn://svn.code.sf.net/p/cxxtools/code/trunk@550 15ae5fad-cc11-0410-8fac-bce609e504b0

---
+++ cxxtools/src/fileinfoimpl.h
@@ -0,0 +1,62 @@
+/***************************************************************************
+ *   Copyright (C) 2006-2008 Marc Boris Duerner                            *
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU Library General Public License as       *
+ *   published by the Free Software Foundation; either version 2 of the    *
+ *   License, or (at your option) any later version.                       *
+ *                                                                         *
+ *   As a special exception, you may use this file as part of a free       *
+ *   software library without restriction. Specifically, if other files    *
+ *   instantiate templates or use macros or inline functions from this     *
+ *   file, or you compile this file and link it with other files to        *
+ *   produce an executable, this file does not by itself cause the         *
+ *   resulting executable to be covered by the GNU General Public          *
+ *   License. This exception does not however invalidate any other         *
+ *   reasons why the executable file might be covered by the GNU Library   *
+ *   General Public License.                                               *
+ *                                                                         *
+ *   This program is distributed in the hope that it will be useful,       *
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of        *
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the         *
+ *   GNU General Public License for more details.                          *
+ *                                                                         *
+ *   You should have received a copy of the GNU Library General Public     *
+ *   License along with this program; if not, write to the                 *
+ *   Free Software Foundation, Inc.,                                       *
+ *   59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.             *
+ ***************************************************************************/
+#include "cxxtools/fileinfo.h"
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <errno.h>
+
+namespace cxxtools {
+
+    class FileInfoImpl
+    {
+        public:
+            static FileInfo::Type getType(const std::string& path)
+            {
+                struct stat st;
+                if( 0 != ::stat(path.c_str(), &st) )
+                {
+                    return FileInfo::Invalid;
+                }
+
+                if( S_ISREG(st.st_mode) )
+                {
+                    return FileInfo::File;
+                }
+                else if( S_ISDIR(st.st_mode) ) 
+                {
+                    return FileInfo::Directory;
+                }
+
+                return FileInfo::File;
+            }
+    };
+
+} // namespace cxxtools
