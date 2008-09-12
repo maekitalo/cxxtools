1a7785901b41c5651f8980fafa7f3f45eba7b61e
maekitalo <maekitalo@15ae5fad-cc11-0410-8fac-bce609e504b0>
2008-09-12 12:03:18 +0000

    add LGPL exception for large templates and macros
    git-svn-id: svn://svn.code.sf.net/p/cxxtools/code/trunk@587 15ae5fad-cc11-0410-8fac-bce609e504b0

--- cxxtools/include/cxxtools/inifile.h
+++ cxxtools/include/cxxtools/inifile.h
@@ -7,6 +7,16 @@
  * published by the Free Software Foundation; either version 2 of the
  * License, or (at your option) any later version.
  *
+ * As a special exception, you may use this file as part of a free
+ * software library without restriction. Specifically, if other files
+ * instantiate templates or use macros or inline functions from this
+ * file, or you compile this file and link it with other files to
+ * produce an executable, this file does not by itself cause the
+ * resulting executable to be covered by the GNU General Public
+ * License. This exception does not however invalidate any other
+ * reasons why the executable file might be covered by the GNU Library
+ * General Public License.
+ * 
  * This program is distributed in the hope that it will be useful, but
  * is provided AS IS, WITHOUT ANY WARRANTY; without even the implied
  * warranty of MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE, and
--- cxxtools/include/cxxtools/regex.h
+++ cxxtools/include/cxxtools/regex.h
@@ -6,6 +6,16 @@
  * License as published by the Free Software Foundation; either
  * version 2.1 of the License, or (at your option) any later version.
  *
+ * As a special exception, you may use this file as part of a free
+ * software library without restriction. Specifically, if other files
+ * instantiate templates or use macros or inline functions from this
+ * file, or you compile this file and link it with other files to
+ * produce an executable, this file does not by itself cause the
+ * resulting executable to be covered by the GNU General Public
+ * License. This exception does not however invalidate any other
+ * reasons why the executable file might be covered by the GNU Library
+ * General Public License.
+ * 
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
