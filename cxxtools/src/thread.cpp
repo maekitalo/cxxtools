3d1af1deb70a06f92001705a00d8d6b72cce5501
maekitalo <maekitalo@15ae5fad-cc11-0410-8fac-bce609e504b0>
2008-11-05 14:40:49 +0000

    remove unused include
    git-svn-id: svn://svn.code.sf.net/p/cxxtools/code/trunk@597 15ae5fad-cc11-0410-8fac-bce609e504b0

--- cxxtools/src/thread.cpp
+++ cxxtools/src/thread.cpp
@@ -28,7 +28,6 @@
  *   59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.             *
  ***************************************************************************/
 #include "threadimpl.h"
-#include "cxxtools/sourceinfo.h"
 #include "cxxtools/thread.h"
 #include <stdexcept>
 
