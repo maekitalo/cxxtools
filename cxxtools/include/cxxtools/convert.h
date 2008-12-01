185e41a31eb7d67fbe550b8e6a49dc28e630cf83
d-marc <d-marc@15ae5fad-cc11-0410-8fac-bce609e504b0>
2008-12-01 07:51:12 +0000

    added conversion algos
    git-svn-id: svn://svn.code.sf.net/p/cxxtools/code/trunk@622 15ae5fad-cc11-0410-8fac-bce609e504b0

---
+++ cxxtools/include/cxxtools/convert.h
@@ -0,0 +1,103 @@
+/***************************************************************************
+ *   Copyright (C) 2004-2007 by Marc Boris Duerner                         *
+ *   Copyright (C) 2004-2007 by Stepan Beal                                *
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU Library General Public License as       *
+ *   published by the Free Software Foundation; either version 2 of the    *
+ *   License, or (at your option) any later version.                       *
+ *                                                                         *
+ *   As a special exception, you may use this file as part of a free       *
+ *   software library without restriction. Specifically, if other files    *
+ *   instantiate templates or use macros or inline functions from this     *
+ *   file, or you compile this file and link it with other files to        *
+ *   produce an executable, this file does not by itself cause the         *
+ *   resulting executable to be covered by the GNU General Public          *
+ *   License. This exception does not however invalidate any other         *
+ *   reasons why the executable file might be covered by the GNU Library   *
+ *   General Public License.                                               *
+ *                                                                         *
+ *   This program is distributed in the hope that it will be useful,       *
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of        *
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the         *
+ *   GNU General Public License for more details.                          *
+ *                                                                         *
+ *   You should have received a copy of the GNU Library General Public     *
+ *   License along with this program; if not, write to the                 *
+ *   Free Software Foundation, Inc.,                                       *
+ *   59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.             *
+ ***************************************************************************/
+#ifndef CXXTOOLS_CONVERT_H
+#define CXXTOOLS_CONVERT_H
+
+#include <cxxtools/sourceinfo.h>
+#include <sstream>
+#include <string>
+#include <stdexcept>
+#include <iomanip>
+#include <limits>
+
+#define CXXTOOLS_CONVERSIONERROR(to, from) \
+    CXXTOOLS_ERROR_MSG("conversion from " #from " to " #to " failed")
+
+namespace cxxtools {
+
+class ConversionError : public std::runtime_error
+{
+    public:
+        ConversionError(const char* msg);
+
+        ~ConversionError() throw()
+        {}
+};
+
+
+template <typename T>
+inline void convert(std::string& s, const T& value)
+{
+    std::ostringstream os;
+    os << value;
+    s = os.str();
+}
+
+
+template <typename T>
+inline void convert(T& t, const std::string& str)
+{
+    std::istringstream is(str);
+    is >> t;
+}
+
+
+template<typename T, typename S>
+void convert(T& to, const S& from)
+{
+    std::stringstream ss;
+    if( !(ss << from && ss >> to) )
+        throw ConversionError( CXXTOOLS_CONVERSIONERROR(streamable, streamable) );
+}
+
+
+template<typename T, typename S>
+struct Convert
+{
+    T operator()(const S& from) const
+    {
+        T value = T();
+        convert(value, from);
+        return value;
+    }
+};
+
+
+template<typename T, typename S>
+T convert(const S& from)
+{
+    T value = T();
+    convert(value, from);
+    return value;
+}
+
+} // namespace cxxtools
+
+#endif
