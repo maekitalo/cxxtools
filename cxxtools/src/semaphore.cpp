fc78e8beb438ff5bb6a4a23a5156a01a5aceec29
d-marc <d-marc@15ae5fad-cc11-0410-8fac-bce609e504b0>
2008-12-18 14:32:25 +0000

    added semaphore class
    git-svn-id: svn://svn.code.sf.net/p/cxxtools/code/trunk@673 15ae5fad-cc11-0410-8fac-bce609e504b0

---
+++ cxxtools/src/semaphore.cpp
@@ -0,0 +1,64 @@
+/*
+ * Copyright (C) 2005 by Marc Boris Duerner
+ * 
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ * 
+ * As a special exception, you may use this file as part of a free
+ * software library without restriction. Specifically, if other files
+ * instantiate templates or use macros or inline functions from this
+ * file, or you compile this file and link it with other files to
+ * produce an executable, this file does not by itself cause the
+ * resulting executable to be covered by the GNU General Public
+ * License. This exception does not however invalidate any other
+ * reasons why the executable file might be covered by the GNU Library
+ * General Public License.
+ * 
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ * 
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
+ */
+#include "semaphoreimpl.h"
+#include "cxxtools/semaphore.h"
+
+namespace cxxtools {
+
+Semaphore::Semaphore(unsigned int initial)
+{
+    _impl = new SemaphoreImpl(initial);
+}
+
+
+Semaphore::~Semaphore()
+{
+    delete _impl;
+}
+
+
+Semaphore& Semaphore::wait()
+{
+    _impl->wait();
+    return *this;
+}
+
+
+bool Semaphore::tryWait()
+{
+    return _impl->tryWait();
+}
+
+
+Semaphore& Semaphore::post()
+{
+    _impl->post();
+    return *this;
+}
+
+} // namespace cxxtools
