2caf73cc0c597ed48367f885ccf35127571cfd3b
maekitalo <maekitalo@15ae5fad-cc11-0410-8fac-bce609e504b0>
2009-06-12 16:58:17 +0000

    add new rpcclient and server demo rpcecho
    git-svn-id: svn://svn.code.sf.net/p/cxxtools/code/trunk@880 15ae5fad-cc11-0410-8fac-bce609e504b0

---
+++ cxxtools/demo/rpcecho.cpp
@@ -0,0 +1,137 @@
+/*
+ * Copyright (C) 2009 Tommi Maekitalo
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License as
+ * published by the Free Software Foundation; either version 2 of the
+ * License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful, but
+ * is provided AS IS, WITHOUT ANY WARRANTY; without even the implied
+ * warranty of MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE, and
+ * NON-INFRINGEMENT.  See the GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301 USA
+ *
+ */
+
+/*
+
+This demo implements a rpc server and a rpc client.
+
+To run the demo start the server with "rpcecho -S". It listens by default on
+port 7002. It waits for incoming xmlrpc requests. The only service function
+is echo, which takes a string as a parameter just returns that string.
+
+To use the server run rpcecho with a text e.g. "rpcecho hello". It sends the
+text to the server and prints the return string to standard out. You may pass
+with -f <filename> a file, which will be sent to the server instead.
+
+*/
+
+#include <iostream>
+#include <cxxtools/arg.h>
+#include <cxxtools/loginit.h>
+#include <cxxtools/xmlrpc/service.h>
+#include <cxxtools/http/server.h>
+#include <cxxtools/xmlrpc/remoteprocedure.h>
+#include <fstream>
+
+////////////////////////////////////////////////////////////////////////
+// This defines a xmlrpc service. A xmlrpc service defines functions, which
+// can be called remotely.
+//
+class EchoServerService : public cxxtools::xmlrpc::Service
+{
+  public:
+    EchoServerService()
+    {
+      registerMethod("echo", *this, &EchoServerService::echo);
+    }
+
+    std::string echo(const std::string& message, bool tostdout);
+};
+
+std::string EchoServerService::echo(const std::string& message, bool tostdout)
+{
+  if (tostdout)
+    std::cout << message << std::endl;
+
+  return message;
+}
+
+////////////////////////////////////////////////////////////////////////
+// main
+//
+int main(int argc, char* argv[])
+{
+  try
+  {
+    log_init();
+
+    cxxtools::Arg<bool> server(argc, argv, 'S');
+
+    cxxtools::Arg<std::string> ip(argc, argv, 'i', server.isSet() ? "0.0.0.0" : "127.0.0.1");
+    cxxtools::Arg<unsigned short> port(argc, argv, 'p', 7002);
+
+    if (server)
+    {
+      std::cout << "run rpcecho server" << std::endl;
+
+      // the http server is instantiated with a ip address and a port number
+      cxxtools::http::Server server(ip, port);
+
+      // we create an instance of the service class
+      EchoServerService service;
+      // ... and register it under a url
+      server.addService("/myservice", service);
+
+      // now run the server
+      server.run();
+    }
+    else
+    {
+      std::cout << "run rpcecho client" << std::endl;
+
+      // take option -n <number> to specify, how often the request should be called (1 by default)
+      cxxtools::Arg<unsigned> number(argc, argv, 'n', 1);
+      cxxtools::Arg<bool> doEcho(argc, argv, 'e');
+
+      // define a xlmrpc client
+      cxxtools::xmlrpc::Client client(ip, port, "/myservice");
+
+      // define remote procedure with std::string return value and a std::string and a bool parameter:
+      cxxtools::xmlrpc::RemoteProcedure<std::string, std::string, bool> echo(client, "echo");
+
+      // optionally pass a filename with -f
+      cxxtools::Arg<const char*> filename(argc, argv, 'f');
+
+      if (filename.isSet())
+      {
+        // read a file into a stringstream
+        std::ifstream in(filename);
+        std::ostringstream data;
+        data << in.rdbuf();  // read file into stringstream
+
+        // send data from file to server and print reply to std::cout
+        for (unsigned n = 0; n < number; ++n)
+          std::cout << echo(data.str(), doEcho);
+      }
+      else
+      {
+        // no filename given, so send just the parameters to the server.
+
+        for (unsigned n = 0; n < number; ++n)
+          for (int a = 1; a < argc; ++a)
+            std::cout << echo(argv[a], doEcho) << std::endl;
+      }
+    }
+  }
+  catch (const std::exception& e)
+  {
+    std::cerr << e.what() << std::endl;
+  }
+}
+
