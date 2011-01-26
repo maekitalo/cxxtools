f5d0a4d465d2e89468ca39018c005870470dc8a4
maekitalo <maekitalo@15ae5fad-cc11-0410-8fac-bce609e504b0>
2011-01-26 10:32:55 +0000

    fix include guard in atomicity.h
    git-svn-id: svn://svn.code.sf.net/p/cxxtools/code/trunk@1196 15ae5fad-cc11-0410-8fac-bce609e504b0

--- cxxtools/include/cxxtools/atomicity.h
+++ cxxtools/include/cxxtools/atomicity.h
@@ -27,6 +27,7 @@
  * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
  */
 #ifndef CXXTOOLS_ATOMICITY_H
+#define CXXTOOLS_ATOMICITY_H
 
 #include <cxxtools/config.h>
 
