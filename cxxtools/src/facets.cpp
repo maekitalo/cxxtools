c88b1dd2ae59e523e9d05b21dee73c9152da3e26
adig33 <adig33@15ae5fad-cc11-0410-8fac-bce609e504b0>
2011-09-19 21:41:38 +0000

    revert to 1252
    git-svn-id: svn://svn.code.sf.net/p/cxxtools/code/trunk@1255 15ae5fad-cc11-0410-8fac-bce609e504b0

--- cxxtools/src/facets.cpp
+++ cxxtools/src/facets.cpp
@@ -26,7 +26,7 @@
  * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
  */
 
-#include <cxxtools/facets.h>
+namespace std {
 
 locale::id numpunct<cxxtools::Char>::id;
 
