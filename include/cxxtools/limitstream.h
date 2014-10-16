a0f599287bafc58ba72fe8bbd08653a637c349a6
Tommi Mäkitalo <tommi@tntnet.org>
2014-10-16 22:43:37 +0200

    new (std::)streams which limit the number of characters to read or write

---
+++ include/cxxtools/limitstream.h
@@ -0,0 +1,269 @@
+/*
+ * Copyright (C) 2014 Tommi Maekitalo
+ *
+ */
+
+#ifndef CXXTOOLS_LIMITSTREAM_H
+#define CXXTOOLS_LIMITSTREAM_H
+
+#include <iostream>
+#include <algorithm>
+
+namespace cxxtools
+{
+    /**
+      The class implements a stream buffer, which reads or writes from another streambuffer
+      but limits the number of characters.
+
+      There are separate counters for reading and writing. They may be reset whenever
+      needed.
+
+      A buffer is used for reading. It is not guaranteed, that characters not yet read
+      from this stream buffer can be read from the underlying stream buffer.
+
+     */
+    template <typename char_type>
+    class BasicLimitStreambuf : public std::basic_streambuf<char_type>
+    {
+            std::basic_streambuf<char_type>* _sb; 
+            unsigned _icount;
+            unsigned _ocount;
+            unsigned _ibufsize;
+            char_type* _ibuffer;
+
+            typedef typename std::basic_streambuf<char_type>::traits_type traits_type;
+            typedef typename std::basic_streambuf<char_type>::int_type int_type;
+
+        public:
+            /// initialize a BasicLimitStreambuf with a underlying stream.
+            BasicLimitStreambuf(std::basic_streambuf<char_type>* sb,
+                unsigned icount, unsigned ocount, unsigned ibufsize = 8192)
+                : _sb(sb),
+                  _icount(icount),
+                  _ocount(ocount),
+                  _ibufsize(ibufsize),
+                  _ibuffer(0)
+            { }
+
+            ~BasicLimitStreambuf()
+            { delete _ibuffer; }
+
+            /// returns the number of remaining characters to read.
+            unsigned icount() const
+            { return _icount; }
+
+            /// sets the number of remaining characters to read.
+            void icount(unsigned c)
+            { _icount = c; }
+
+            /// returns the number of remaining characters to write.
+            unsigned ocount() const
+            { return _ocount; }
+
+            /// sets the number of remaining characters to write.
+            void ocount(unsigned c)
+            { _ocount = c; }
+
+            /// returns the underlying stream buffer.
+            std::basic_streambuf<char_type>* streambuf()
+            { return _sb; }
+
+            /// sets the underlying stream buffer.
+            void attach(std::basic_streambuf<char_type>* s)
+            { _sb = s; }
+
+        protected:
+            int_type overflow(int_type ch)
+            {
+                if (ch != traits_type::eof() && _ocount > 0)
+                {
+                    int r = _sb->sputc(traits_type::to_char_type(ch));
+                    if (r == traits_type::eof())
+                        return traits_type::eof();
+                    --_ocount;
+                    return traits_type::not_eof(ch);
+                }
+
+                return traits_type::eof();
+            }
+
+            int_type underflow()
+            {
+                if (_icount == 0)
+                    return traits_type::eof();
+
+                if (_ibuffer == 0)
+                    _ibuffer = new char_type[_ibufsize];
+
+                std::streamsize c = _sb->sgetn(_ibuffer, std::min(_icount, _ibufsize));
+                if (c == 0)
+                    return traits_type::eof();
+
+                _icount -= c;
+
+                std::basic_streambuf<char_type>::setg(_ibuffer, _ibuffer, _ibuffer + c);
+
+                return std::basic_streambuf<char_type>::sgetc();
+            }
+
+            int sync()
+            {
+                return 0;
+            }
+    };
+
+    /**
+       The class implements a input stream, which limits the number of characters to read.
+
+       When trying to read more than the specified number of characters, eof is reported.
+    */
+    template <typename char_type>
+    class BasicLimitIStream : public std::basic_istream<char_type>
+    {
+            BasicLimitStreambuf<char_type> _streambuf;
+
+        public:
+            /// Initialize a limiting input stream using a underlying input stream
+            /// and the number of characters to read.
+            BasicLimitIStream(std::basic_istream<char_type>& source, unsigned count)
+                : std::basic_istream<char_type>(0),
+                 _streambuf(source.rdbuf(), count, 0)
+            {
+                std::basic_istream<char_type>::init(&_streambuf);
+            }
+
+            /// Initialize a limiting input stream using a underlying stream buffer
+            /// and the number of characters to read.
+            BasicLimitIStream(std::basic_streambuf<char_type>& source, unsigned count)
+                : std::basic_istream<char_type>(0),
+                 _streambuf(&source, count, 0)
+            {
+                std::basic_istream<char_type>::init(&_streambuf);
+            }
+
+            /// returns the number of remaining characters to read.
+            unsigned icount() const
+            { return _streambuf.icount(); }
+
+            /// sets the number of remaining characters to read.
+            void icount(unsigned c)
+            { _streambuf.icount(c); }
+
+            /// sets the underlying stream buffer.
+            void attach(std::basic_istream<char_type>& s)
+            { _streambuf.attach(s.rdbuf()); }
+
+            /// sets the underlying stream buffer.
+            void attach(std::basic_streambuf<char_type>* s)
+            { _streambuf.attach(s); }
+
+    };
+
+    /**
+       The class implements a output stream, which limits the number of characters to write.
+
+       When trying to write more than the specified number of characters, eof is reported.
+     */
+    template <typename char_type>
+    class BasicLimitOStream : public std::basic_ostream<char_type>
+    {
+            BasicLimitStreambuf<char_type> _streambuf;
+
+        public:
+            /// Initialize a limiting output stream using a underlying output stream
+            /// and the number of characters to write.
+            BasicLimitOStream(std::basic_ostream<char_type>& sink, unsigned count)
+                : std::basic_ostream<char_type>(0),
+                 _streambuf(sink.rdbuf(), 0, count)
+            {
+                std::basic_ostream<char_type>::init(&_streambuf);
+            }
+
+            /// Initialize a limiting output stream using a underlying stream buffer
+            /// and the number of characters to write.
+            BasicLimitOStream(std::basic_streambuf<char_type>& sink, unsigned count)
+                : std::basic_ostream<char_type>(0),
+                 _streambuf(&sink, 0, count)
+            {
+                std::basic_ostream<char_type>::init(&_streambuf);
+            }
+
+            /// sets the number of remaining characters to write.
+            unsigned ocount() const
+            { return _streambuf.ocount(); }
+
+            /// returns the number of remaining characters to write.
+            void ocount(unsigned c)
+            { _streambuf.ocount(c); }
+
+            /// sets the underlying stream buffer.
+            void attach(std::basic_ostream<char_type>& s)
+            { _streambuf.attach(s.rdbuf()); }
+
+            /// sets the underlying stream buffer.
+            void attach(std::basic_streambuf<char_type>* s)
+            { _streambuf.attach(s); }
+
+    };
+
+    /**
+       This class implements a input/output stream, which limits the number of characters to read and write.
+     */
+    template <typename char_type>
+    class BasicLimitIOStream : public std::basic_iostream<char_type>
+    {
+            BasicLimitStreambuf<char_type> _streambuf;
+
+        public:
+            /// Initialize a limiting I/O stream using a underlying output stream,
+            /// the number of characters to read and the number of characters to write.
+            BasicLimitIOStream(std::basic_iostream<char_type>& sinksource, unsigned icount, unsigned ocount)
+                : std::basic_iostream<char_type>(0),
+                 _streambuf(sinksource.rdbuf(), icount, ocount)
+            {
+                std::basic_iostream<char_type>::init(&_streambuf);
+            }
+
+            /// Initialize a limiting I/O stream using a underlying stream buffer,
+            /// the number of characters to read and the number of characters to write.
+            BasicLimitIOStream(std::basic_streambuf<char_type>& sinksource, unsigned icount, unsigned ocount)
+                : std::basic_iostream<char_type>(0),
+                 _streambuf(&sinksource, icount, ocount)
+            {
+                std::basic_iostream<char_type>::init(&_streambuf);
+            }
+
+            /// returns the number of remaining characters to read.
+            unsigned icount() const
+            { return _streambuf.icount(); }
+
+            /// sets the number of remaining characters to read.
+            void icount(unsigned c)
+            { _streambuf.icount(c); }
+
+            /// returns the number of remaining characters to write.
+            unsigned ocount() const
+            { return _streambuf.ocount(); }
+
+            /// sets the number of remaining characters to write.
+            void ocount(unsigned c)
+            { _streambuf.ocount(c); }
+
+            /// sets the underlying stream buffer.
+            void attach(std::basic_iostream<char_type>& s)
+            { _streambuf.attach(s.rdbuf()); }
+
+            /// sets the underlying stream buffer.
+            void attach(std::basic_streambuf<char_type>* s)
+            { _streambuf.attach(s); }
+
+    };
+
+    typedef BasicLimitStreambuf<char> LimitStreambuf;
+    typedef BasicLimitIStream<char> LimitIStream;
+    typedef BasicLimitOStream<char> LimitOStream;
+    typedef BasicLimitIOStream<char> LimitIOStream;
+}
+
+#endif // CXXTOOLS_LIMITSTREAM_H
+
---
+++ test/limitstream-test.cpp
@@ -0,0 +1,84 @@
+/*
+ * Copyright (C) 2014 Tommi Maekitalo
+ * 
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ * 
+ * As a special exception, you may use this file as part of a free
+ * software library without restriction. Specifically, if other files
+ * instantiate templates or use macros or inline functions from this
+ * file, or you compile this file and link it with other files to
+ * produce an executable, this file does not by itself cause the
+ * resulting executable to be covered by the GNU General Public
+ * License. This exception does not however invalidate any other
+ * reasons why the executable file might be covered by the GNU Library
+ * General Public License.
+ * 
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ * 
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
+ */
+
+#include "cxxtools/limitstream.h"
+#include "cxxtools/unit/testsuite.h"
+#include "cxxtools/unit/registertest.h"
+#include <sstream>
+
+class LimitStreamTest : public cxxtools::unit::TestSuite
+{
+    public:
+        LimitStreamTest()
+        : cxxtools::unit::TestSuite("limitstream")
+        {
+            registerMethod("testIStream", *this, &LimitStreamTest::testIStream);
+            registerMethod("testOStream", *this, &LimitStreamTest::testOStream);
+        }
+
+        void testIStream()
+        {
+            std::istringstream is("foobar");
+            cxxtools::LimitIStream lis(is, 3);
+
+            std::string s;
+            lis >> s;
+            CXXTOOLS_UNIT_ASSERT(lis.eof());
+            CXXTOOLS_UNIT_ASSERT_EQUALS(s, "foo");
+
+            lis.icount(5);
+            lis.clear();
+            lis >> s;
+            CXXTOOLS_UNIT_ASSERT(lis.eof());
+            CXXTOOLS_UNIT_ASSERT_EQUALS(lis.icount(), 2);
+            CXXTOOLS_UNIT_ASSERT_EQUALS(s, "bar");
+        }
+
+        void testOStream()
+        {
+            std::ostringstream os;
+            cxxtools::LimitOStream los(os, 3);
+
+            los << "foobar";
+            CXXTOOLS_UNIT_ASSERT_EQUALS(os.str(), "foo");
+            CXXTOOLS_UNIT_ASSERT_EQUALS(los.ocount(), 0);
+
+            los.ocount(5);
+            los.clear();
+
+            los << "bar";
+            CXXTOOLS_UNIT_ASSERT_EQUALS(os.str(), "foobar");
+            CXXTOOLS_UNIT_ASSERT_EQUALS(los.ocount(), 2);
+
+            los << "baz";
+            CXXTOOLS_UNIT_ASSERT_EQUALS(os.str(), "foobarba");
+            CXXTOOLS_UNIT_ASSERT_EQUALS(los.ocount(), 0);
+        }
+};
+
+cxxtools::unit::RegisterTest<LimitStreamTest> register_LimitStreamTest;
