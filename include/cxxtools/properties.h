9e88135ad293c1d1987e62a40c20f5560b9ab72a
Tommi Mäkitalo <tommi@tntnet.org>
2015-09-09 14:52:54 +0200

    implement propertiesserializer

--- include/cxxtools/properties.h
+++ include/cxxtools/properties.h
@@ -30,16 +30,68 @@
 #define CXXTOOLS_PROPERTIES_H
 
 #include <cxxtools/propertiesdeserializer.h>
+#include <cxxtools/propertiesserializer.h>
 #include <iostream>
 
 namespace cxxtools
 {
     /**
+       Wrapper object to easily print serializable objects as properties to a output stream.
+
+       PropertiesOObject is a little wrapper which makes it easy to output serializable
+       objects into a ostream. For this the PropertiesOObject expects a reference to the
+       wrapped object and has a output operator for a std::ostream, or actually
+       a std::basic_ostream, which prints the object in properties format.
+
+       Example:
+       \code
+        std::vector<unsigned> v;
+        v.push_back(4);
+        v.push_back(17);
+        v.push_back(12);
+        std::cout << cxxtools::Properties(v).beautify(true) << std::endl;
+       \endcode
+     */
+    template <typename ObjectType>
+    class PropertiesOObject
+    {
+        const ObjectType& _constObject;
+
+      public:
+        /// Constructor. Needs the wrapped object. Optionally a flag can be
+        /// passed whether the properties should be nicely formatted.
+        explicit PropertiesOObject(const ObjectType& object, bool beautify = false)
+          : _constObject(object)
+        { }
+
+        const ObjectType& object() const
+        { return _constObject; }
+    };
+
+    /// The output operator for PropertiesOObject. It does the actual work.
+    template <typename CharType, typename ObjectType>
+    std::basic_ostream<CharType>& operator<< (std::basic_ostream<CharType>& out, const PropertiesOObject<ObjectType>& object)
+    {
+      try
+      {
+        PropertiesSerializer serializer(out);
+        serializer.serialize(object.object())
+                  .finish();
+      }
+      catch (const std::exception&)
+      {
+        out.setstate(std::ios::failbit);
+        throw;
+      }
+      return out;
+    }
+
+    /**
        Wrapper object to easily read properties from a input stream and convert them to serializable objects.
 
      */
     template <typename ObjectType>
-    class PropertiesIObject
+    class PropertiesIOObject : public PropertiesOObject<ObjectType>
     {
         ObjectType& _object;
         std::string _name;
@@ -48,8 +100,9 @@ namespace cxxtools
 
       public:
         /// Constructor. Needs the wrapped object.
-        explicit PropertiesIObject(ObjectType& object, const std::string& name = std::string())
-          : _object(object),
+        explicit PropertiesIOObject(ObjectType& object, const std::string& name = std::string())
+          : PropertiesOObject<ObjectType>(object),
+            _object(object),
             _name(name),
             _envSubst(false),
             _trim(false)
@@ -64,7 +117,7 @@ namespace cxxtools
         /// Sets the envSubst flag.
         /// When set flag is set, environment variables are substituted in the
         /// value using / `cxxtools::envSubst`.
-        PropertiesIObject& envSubst(bool sw) { _envSubst = sw; return *this; }
+        PropertiesIOObject& envSubst(bool sw) { _envSubst = sw; return *this; }
 
         /// Returns the envSubst flag.
         bool envSubst() const
@@ -73,7 +126,7 @@ namespace cxxtools
         /// Sets the trim flag.
         /// When set flag is set, white space is removed from the beginning
         /// and end of the values.
-        PropertiesIObject& trim(bool sw)
+        PropertiesIOObject& trim(bool sw)
         { _trim = sw; return *this; }
 
         /// Returns the trim flag.
@@ -82,9 +135,9 @@ namespace cxxtools
 
     };
 
-    /// The input operator for PropertiesIObject. It does the actual work.
+    /// The input operator for PropertiesIOObject. It does the actual work.
     template <typename CharType, typename ObjectType>
-    std::basic_istream<CharType>& operator>> (std::basic_istream<CharType>& in, PropertiesIObject<ObjectType> object)
+    std::basic_istream<CharType>& operator>> (std::basic_istream<CharType>& in, PropertiesIOObject<ObjectType> object)
     {
       PropertiesDeserializer deserializer;
       deserializer.envSubst(object.envSubst());
@@ -98,9 +151,9 @@ namespace cxxtools
     }
 
     template <typename ObjectType>
-    PropertiesIObject<ObjectType> Properties(ObjectType& object, const std::string& name = std::string())
+    PropertiesIOObject<ObjectType> Properties(ObjectType& object, const std::string& name = std::string())
     {
-      return PropertiesIObject<ObjectType>(object, name);
+      return PropertiesIOObject<ObjectType>(object, name);
     }
 
 }
---
+++ include/cxxtools/propertiesserializer.h
@@ -0,0 +1,110 @@
+/*
+ * Copyright (C) 2015 Tommi Maekitalo
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * As a special exception, you may use this file as part of a free
+ * software library without restriction. Specifically, if other files
+ * instantiate templates or use macros or inline functions from this
+ * file, or you compile this file and link it with other files to
+ * produce an executable, this file does not by itself cause the
+ * resulting executable to be covered by the GNU General Public
+ * License. This exception does not however invalidate any other
+ * reasons why the executable file might be covered by the GNU Library
+ * General Public License.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
+ */
+
+#ifndef CXXTOOLS_PROPERTIESSERIALIZER_H
+#define CXXTOOLS_PROPERTIESSERIALIZER_H
+
+#include <cxxtools/textstream.h>
+#include <cxxtools/serializationinfo.h>
+
+namespace cxxtools
+{
+    class PropertiesSerializer
+    {
+#if __cplusplus >= 201103L
+            PropertiesSerializer(const PropertiesSerializer&) = delete;
+            PropertiesSerializer& operator=(const PropertiesSerializer&) = delete;
+#else
+            PropertiesSerializer(const PropertiesSerializer&) { }
+            PropertiesSerializer& operator=(const PropertiesSerializer&) { return *this; }
+#endif
+
+        public:
+            PropertiesSerializer()
+                : _myts(0),
+                  _ts(0)
+            {
+            }
+
+            explicit PropertiesSerializer(std::basic_ostream<cxxtools::Char>& ts)
+                : _myts(0),
+                  _ts(0)
+            {
+                begin(ts);
+            }
+
+            explicit PropertiesSerializer(std::ostream& os,
+                TextCodec<cxxtools::Char, char>* codec = 0)
+                : _myts(0),
+                  _ts(0)
+            {
+                begin(os, codec);
+            }
+
+            ~PropertiesSerializer()
+            {
+                delete _myts;
+            }
+
+            PropertiesSerializer& begin(std::basic_ostream<cxxtools::Char>& ts);
+
+            PropertiesSerializer& begin(std::ostream& os,
+                TextCodec<cxxtools::Char, char>* codec = 0);
+
+            void finish();
+
+            template <typename T>
+            PropertiesSerializer& serialize(const T& v, const std::string& name)
+            {
+                SerializationInfo si;
+                si <<= v;
+                format(si, name);
+                return *this;
+            }
+
+            template <typename T>
+            PropertiesSerializer& serialize(const T& v)
+            {
+                SerializationInfo si;
+                si <<= v;
+                format(si, si.name());
+                return *this;
+            }
+
+        private:
+            void format(const SerializationInfo& si, const std::string& key);
+            void doFormat(const SerializationInfo& si, const std::string& key);
+
+            std::basic_ostream<cxxtools::Char>* _myts;
+            std::basic_ostream<cxxtools::Char>* _ts;
+    };
+
+}
+
+#endif // CXXTOOLS_PROPERTIESSERIALIZER_H
+
