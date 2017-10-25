25c0c09b55f63816b217b7ab1ef2751f7fd7ef65
Tommi Mäkitalo <tommi@tntnet.org>
2017-10-25 21:24:13 +0200

    add some demo/test programs for client certificates

---
+++ demo/ssl/rpcecho.cpp
@@ -0,0 +1,46 @@
+#include <iostream>
+#include <cxxtools/arg.h>
+#include <cxxtools/log.h>
+#include <cxxtools/sslcertificate.h>
+#include <cxxtools/remoteprocedure.h>
+#include <cxxtools/bin/rpcclient.h>
+
+bool noAccept(const cxxtools::SslCertificate& cert)
+{
+    std::cout << "cert \"" << cert.getSubject() << "\" not accepted" << std::endl;
+    return false;
+}
+
+////////////////////////////////////////////////////////////////////////
+// main
+//
+int main(int argc, char* argv[])
+{
+  try
+  {
+    log_init();
+
+    cxxtools::Arg<std::string> ip(argc, argv, 'i');
+    cxxtools::Arg<unsigned short> port(argc, argv, 'p', 7003);
+    cxxtools::Arg<std::string> cert(argc, argv, 'c');
+    cxxtools::Arg<bool> deny(argc, argv, 'd');
+
+    cxxtools::bin::RpcClient client(ip, port, cert);
+
+    if (deny)
+        cxxtools::connect(client.acceptSslCertificate(), noAccept);
+
+    cxxtools::RemoteProcedure<std::string, std::string> echo(client, "echo");
+
+    for (int a = 1; a < argc; ++a)
+    {
+      std::string v = echo(argv[a]);
+      std::cout << v << '\n';
+    }
+  }
+  catch (const std::exception& e)
+  {
+    std::cerr << e.what() << std::endl;
+  }
+}
+
---
+++ demo/ssl/rpcjecho.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+#include <cxxtools/arg.h>
+#include <cxxtools/log.h>
+#include <cxxtools/remoteprocedure.h>
+#include <cxxtools/json/rpcclient.h>
+
+////////////////////////////////////////////////////////////////////////
+// main
+//
+int main(int argc, char* argv[])
+{
+  try
+  {
+    log_init();
+
+    cxxtools::Arg<std::string> ip(argc, argv, 'i');
+    cxxtools::Arg<unsigned short> port(argc, argv, 'p', 7004);
+    cxxtools::Arg<std::string> cert(argc, argv, 'c');
+
+    cxxtools::json::RpcClient client(ip, port, cert);
+
+    cxxtools::RemoteProcedure<std::string, std::string> echo(client, "echo");
+
+    for (int a = 1; a < argc; ++a)
+    {
+      std::string v = echo(argv[a]);
+      std::cout << v << '\n';
+    }
+  }
+  catch (const std::exception& e)
+  {
+    std::cerr << e.what() << std::endl;
+  }
+}
+
