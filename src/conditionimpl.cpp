2c9817f4396ed752d0becd6f8f84b72c1297e957
Tommi Mäkitalo <tommi@tntnet.org>
2020-02-05 07:19:17 +0100

    fix timed wait for condition
    Time was not calculated correctly and hence the wait may return too early

--- src/conditionimpl.cpp
+++ src/conditionimpl.cpp
@@ -73,7 +73,7 @@ bool ConditionImpl::wait(Mutex& mtx, const Timespan& ts)
     struct timespec tv;
 
     tv.tv_sec = tt.totalUSecs() / 1000000;
-    tv.tv_nsec = tt.totalUSecs() % 1000000;
+    tv.tv_nsec = (tt.totalUSecs() % 1000000) * 1000;
 
     do
     {
