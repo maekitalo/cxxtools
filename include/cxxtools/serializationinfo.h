maekitalo/cxxtools

commit 8b731cc715b24d9f67dae336e60d7b9c52fe130a

cxxtools::SerializationInfo::clear deletes sub nodes

include/cxxtools/serializationinfo.h
@@ -221,7 +221,7 @@ class SerializationInfo
         void setValue(float value)               { _setFloat(value); }
         void setValue(double value)              { _setDouble(value); }
         void setValue(long double value)         { _setLongDouble(value); }
-        void setNull();
+        void setNull()                           { clear(); }
 
         /** @brief Deserialization of flat data-types
         */

src/serializationinfo.cpp
@@ -304,8 +304,8 @@ void SerializationInfo::clear()
     _category = Void;
     _name.clear();
     _type.clear();
-    if (_nodes)
-        _nodes->clear();
+    delete _nodes;
+    _nodes = nullptr;
     _releaseValue();
 }
 
@@ -757,13 +757,6 @@ void SerializationInfo::_releaseValue()
             ;
     }
     _t = t_none;
-}
-
-void SerializationInfo::setNull()
-{
-    _releaseValue();
-    _t = t_none;
-    _category = Value;
 }
 
 void SerializationInfo::_setString(String&& value)

commit 2856a8e0aa3b4051f7ef525ac4d02530715163a6

Fix null value handling in SerializationInfo
This was broken with 8b731cc715b24d9f67dae336e60d7b9c52fe130a
setting SerializationInfo to null should not clear everything but just
set the value to null and deleting subnodes but must not empty the name
and type of the node.

include/cxxtools/serializationinfo.h
@@ -221,7 +221,7 @@ class SerializationInfo
         void setValue(float value)               { _setFloat(value); }
         void setValue(double value)              { _setDouble(value); }
         void setValue(long double value)         { _setLongDouble(value); }
-        void setNull()                           { clear(); }
+        void setNull();
 
         /** @brief Deserialization of flat data-types
         */

src/serializationinfo.cpp
@@ -759,6 +759,15 @@ void SerializationInfo::_releaseValue()
     _t = t_none;
 }
 
+void SerializationInfo::setNull()
+{
+    _releaseValue();
+    _t = t_none;
+    _category = Value;
+    delete _nodes;
+    _nodes = nullptr;
+}
+
 void SerializationInfo::_setString(String&& value)
 {
     if (_t != t_string)